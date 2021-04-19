4ddd34e607573e1c5ea4a7fdf44a3910ae4f635b
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-04-19 12:29:10 +0600

    working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
-
+const ObjectID = require('mongodb').ObjectID;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sot4y.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
@@ -19,7 +19,9 @@ app.get('/', (req, res) => {
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const servicesCollection = client.db("crystalEvents").collection("services");
+  const reviewsCollection = client.db("crystalEvents").collection("reviews");
   console.log('database connection successfully')
+
   app.get('/services', (req, res) => {
     servicesCollection.find()
     .toArray((err, items) => {
@@ -28,6 +30,15 @@ client.connect(err => {
     })
   })
 
+  app.get('/review', (req, res) => {
+    reviewsCollection.find()
+    .toArray((err, items) => {
+        res.send(items)
+        console.log('from database', items)
+    })
+  })
+
+
   app.post('/addServices', (req,res) => {
       const service = req.body;
       servicesCollection.insertOne(service)
@@ -35,6 +46,22 @@ client.connect(err => {
           res.send(result.insertedCount > 0)
       })
   })
+  
+  app.post('/addReview', (req,res) => {
+    const service = req.body;
+    reviewsCollection.insertOne(service)
+    .then(result => {
+        res.send(result.insertedCount > 0)
+    })
+})
+
+app.delete('/delete/:id', (req, res) => {
+  const id = ObjectID(req.params.id)
+  console.log('delete this',id)
+  servicesCollection.findOneAndDelete({_id: id})
+  .then(documents => res.send(!!documents.value))
+})
+
 
 });
 
